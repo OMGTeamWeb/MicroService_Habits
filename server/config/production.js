efcf895f625e6b5297116a97c1945b2ffa5d63f8
Luis Arturo Mendoza <arturo10.mr@gmail.com>
2018-04-23 22:32:56 -0500

    mLab Added

--- server/config/production.js
+++ server/config/production.js
@@ -1,9 +1,9 @@
 module.exports = {
   // disbable logging for production
-  logging: false,
+  logging: true,
   db: {
     //crear BD
-    url: 'mongodb://test:test@ds149309.mlab.com:49309/heroku_kcb7x1h5'
+    url: 'mongodb://test:test@ds157089.mlab.com:57089/heroku_29nn32p2'
     //url: 'mongodb://localhost/funmath-dev'
   }
 };
